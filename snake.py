7a5857cf450c4d875f2afe390064be4048b4a7f2
João S. O. Bueno <gwidion@gmail.com>
2015-09-16 12:36:44 -0300

    Refatoração

--- snake.py
+++ snake.py
@@ -4,16 +4,28 @@
 import pygame
 from pygame.locals import *
 
-TAMANHO = 640, 480
+TAMANHO_TELA = 640, 480
 
 def inicio():
     global tela
-    tela = pygame.display.set_mode(TAMANHO)
+    tela = pygame.display.set_mode(TAMANHO_TELA)
+
+
+def desenha(pos, tamanho):
+    pygame.draw.rect(tela, (255,0,0), (pos[0], pos[1],tamanho, tamanho))
+    pygame.display.flip()
 
 def principal():
-    pygame.draw.rect(tela, (255,0,0), (0,0,50, 50))
-    pygame.display.flip()
-    pygame.time.delay(2000)
+    atraso = 30
+    tamanho = 50
+    x, y = 100, 100
+    contador = 0
+    while True:
+        desenha((x,y), tamanho)
+        pygame.time.delay(30)
+        if contador >= 100:
+            break
+        contador += 1
 
 def fim():
     pygame.quit()
